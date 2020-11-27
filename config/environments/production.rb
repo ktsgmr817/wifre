1728d481f2c446321327f11a71fdcc4e06d6eeea
Kota <kouta.s0817@icloud.com>
2020-11-28 00:32:14 +0900

    本番環境mailer設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  #mailer_setting
   config.action_mailer.default_url_options = { host: 'dashboard.heroku.com/apps/wifre'}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :user_name => 'wifre.info@gmail.com',
+    :password => 'wqmfqauycfkbhrms',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
